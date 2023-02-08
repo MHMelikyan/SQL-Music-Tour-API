c4fe2962f3e6104f51c83d1668adee406b9496b9
Melik Melikyan <mhmelikyan@gmail.com>
2023-02-07 20:24:42 -0500

    Associations and Relationship

--- models/band.js
+++ models/band.js
@@ -1,41 +1,62 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Event extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Stage, StageEvent, MeetGreet, SetTime }) {
+      // stages
+      Event.belongsToMany(Stage, {
+        foreignKey: "event_id",
+        as: "stages",
+        through: StageEvent
+      })
+      
+      // meet and greets 
+      Event.hasMany(MeetGreet, {
+        foreignKey: "event_id",
+        as: "meet_greets"
+      })
 
-const { Model, DataTypes}= require('sequelize');
-module.exports = (sequelize, DataTypes)=>{
-    class Band extends Model{
-        static associate(models){
-            
-        }
+      // set times 
+      Event.hasMany(SetTime, {
+        foreignKey: "event_id",
+        as: "set_times"
+      })
     }
-
-
-Band.init({
-    band_id:{ 
-        type: DataTypes.INTEGER,
-         primaryKey: true,
-        autoIncrement: true
+  }
+  Event.init({
+    event_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
     },
-    name:{
-        type: DataTypes.STRING, 
-        allowNull: false 
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
-    genre: { 
-        type: DataTypes.TEXT, 
-        allowNull: false 
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false
     },
-    available_start_time: { 
-        type: DataTypes.DATE, 
-        allowNull: false 
+    start_time: {
+      type: DataTypes.DATE,
+      allowNull: false
     },
-    end_time: { 
-        type: DataTypes.DATE, 
-        allowNull: false 
-    },
-}, {
-    sequelize,                           
-    modelName: 'Band',
-    tableName: 'band',
+    end_time: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Event',
+    tableName: 'events',
     timestamps: false
-
-})
-return Band
+  })
+  return Event
 }
